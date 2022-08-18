4c793cd659f02072d260b325cd8cbd859230c5c6
dds git <deeann@stelzl.us>
2022-08-18 19:58:20 -0400

    wrote README and corrected typos and cleaned up code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# password-generator
+# Project Name: Password Generator
+
+## Description
+[Deployed Site](https://dstelzl.github.io/password-generator/)
+
+I created an application that users can generate random passwords using a combination of upper and lowercase letters, numbers and special characters. Using pre-existing code as a start to the project- I added variables that identified what the character sets could be as well as functions that selected random characters based on the users selection of type of character and password length. I console.log(ged) along the way to make sure the application was operating correctly and captured those results in the visual below.
+
+## Visuals
+![Screenshot including console](./assets/passwordGeneratorScreenshot.jpg)
+
+## Contributions
+created by Deborah Stelzl
+
--- script.js
+++ script.js
@@ -1,81 +1,62 @@
-   
-
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-  //if (passlength <8){
- //alert("Must be at least 8 characters.")
- // }
- // if (passlength >128){
-  //  alert("Must be less than 128 characters")
- // }
 
 
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-console.log(generateBtn)
-var upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-var lower = 'abcdefghijklmnopqrstuvwxyz'
-var numbers = '0123456789'
-var char = '"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~"'
-console.log(char[0])
-// Write password to the #password input
+
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var char = '"#$%&\'()*+,-./:;<=>?@[]^_`{|}~"';
+console.log(char[0]);
 
 
 function writePassword() {
-  console.log("click")
+  console.log("click");
   var password = generatePassword();
-  console.log(password)
+  console.log(password);
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
+function generatePassword() {
+  console.log("generating password");
+  var possibleChars = "";
+  var password = "";
 
- 
+  var passLength = prompt("How long would you like your password to be?");
+  console.log(passLength);
 
-function generatePassword(){
- 
-  console.log('generating password')
-var possibleChars = ""
-var password = ""
-
-  var passLength = prompt('How long would you like your password to be?')
-  console.log(passLength)
-
-  var lCase = confirm ('Do you want to include lowercase letters')
-  if (lCase){
-    possibleChars = possibleChars + lower
+  var lCase = confirm("Do you want to include lowercase letters");
+  if (lCase) {
+    possibleChars = possibleChars + lower;
   }
-  console.log(possibleChars)
-  var uCase = confirm ('Do you want to include Uppercase letters')
-  if (uCase){
-    possibleChars = possibleChars + upper
+  console.log(possibleChars);
+  var uCase = confirm("Do you want to include Uppercase letters");
+  if (uCase) {
+    possibleChars = possibleChars + upper;
   }
-  console.log(possibleChars)
-  var num = confirm ('Do you want to include numbers')
-  if (num){
-    possibleChars += numbers
+  console.log(possibleChars);
+  var num = confirm("Do you want to include numbers");
+  if (num) {
+    possibleChars += numbers;
   }
-  console.log(possibleChars)
-  var specChar = confirm ('Do you want to include specila characters')
-  if (specChar){
-    possibleChars = possibleChars + char
+  console.log(possibleChars);
+  var specChar = confirm("Do you want to include special characters");
+  if (specChar) {
+    possibleChars = possibleChars + char;
   }
-  console.log(possibleChars)
+  console.log(possibleChars);
 
-  console.log(lCase, uCase, num, specChar)
+  console.log(lCase, uCase, num, specChar);
 
-for (var i=0; i<passLength; i++){
-  console.log(i)
-  var randomIndex = Math.floor(Math.random()* possibleChars.length)
-  console.log(randomIndex)
-  console.log(possibleChars[randomIndex])
-password += (possibleChars[randomIndex])
-console.log(password)
-}
+  for (var i = 0; i < passLength; i++) {
+    console.log(i);
+    var randomIndex = Math.floor(Math.random() * possibleChars.length);
+    console.log(randomIndex);
+    console.log(possibleChars[randomIndex]);
+    password += possibleChars[randomIndex];
+  }
   return password;
 }
 
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
